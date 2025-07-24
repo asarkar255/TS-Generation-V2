5127dc04a022f7f330f94c8bc541af81111c503b
Aparup Sarkar <aparup.s.sarkar@pwc.com>
2025-07-24 21:18:05 +0530

    Commit Aparup V09

--- app/ts_generator.py
+++ app/ts_generator.py
@@ -37,12 +37,15 @@ def extract_abap_explanation(abap_code: str) -> str:
         SystemMessage(content="You are an experienced SAP Techno-Functional Solution Architect. "
                               "Explain the given ABAP code line-by-line in detail from both a technical and functional perspective."
                               "Output should be like below:\n"
+                              "Selection Screen Parameters:\n"
+                              "Technical: Technical Explanation of selection screen parameters\n"
                               "SAP Code Line 1:\n"
                               "Technical: Technical Explanation of line 1\n"
                               "Functional: Functional Explanation of line 1\n"
                               "SAP Code Line 2:\n"
                               "Technical: Technical Explanation of line 2\n"  
-                              "Functional: Functional Explanation of line 2\n"  ),
+                              "Functional: Functional Explanation of line 2\n"
+                                ),
         HumanMessage(content=abap_code)
     ]
 
@@ -67,18 +70,20 @@ def generate_ts_from_abap(abap_code: str) -> str:
         return "No relevant context found in the RAG base. Please verify the ABAP code or knowledge file."
 
     # Combine context
-    combined_context = f"### Technical & Functional Explanation:\n{explanation}\n\n### RAG Knowledge Base Context:\n{retrieved_context}"
+
 
     # Prompt to generate TSD
     prompt_template = ChatPromptTemplate.from_template(
-        "You are an SAP ABAP Technical Architect. Based on the following explanation, RAG context "
+        "You are an SAP ABAP Technical Architect. Based on the following explanation, RAG contextand ABAP code, "
         "generate a detailed and professionally formatted Technical Specification Document (minimum 2000 words) "
-        "Use all lines from {explanation} to create Pseudo Code section of TSD."
+        "Use all lines from explanation to create Pseudo Code section of TSD."
         "with DOCX-compatible formatting, section titles, and numbering.\n\n"
         "Context:\n{context}\n\n"
+        "ABAP Code:\n{abap_code}"
+        "Explanation:\n{explanation}"
     )
 
-    messages = prompt_template.format_messages(context=combined_context, abap_code=abap_code, explanation=explanation)
+    messages = prompt_template.format_messages(context=retrieved_context , abap_code=abap_code, explanation=explanation)
 
     # Call LLM
     llm = ChatOpenAI(model="gpt-4o", temperature=0.4)
